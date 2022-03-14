d79c02c0b33db9b13a53a3a4cc3ef63dd54333b1
wanghongda-0624 <73088093+wanghongda-0624@users.noreply.github.com>
2022-03-14 17:23:33 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,3 +30,14 @@ module "vpc" {
   sk=var.sk
   region = var.region
 }
+
+terraform {
+    backend "s3"{
+     access_key = var.ak
+     secret_key = var.sk
+     bucket = var.bucket
+     key    = var.key
+     region = var.region
+     endpoint = var.endpoint 
+  }
+}
