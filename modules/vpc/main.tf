3377a52053fb59d0fd24a5ec7c6275817169dcef
wanghongda-0624 <73088093+wanghongda-0624@users.noreply.github.com>
2022-03-14 17:22:43 +0800

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -10,14 +10,3 @@ resource "huaweicloud_vpc" "example" {
     name = var.name
     cidr = var.cidr
   }
-
-  terraform {
-    backend "s3"{
-     access_key = var.ak
-     secret_key = var.sk
-     bucket = var.bucket
-     key    = var.key
-     region = var.region
-     endpoint = var.endpoint 
-  }
-}
